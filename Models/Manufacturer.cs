dc8c213f5cf07c221909a93fdfd5ad49d8369491
Mohakkapoor98 <200425207@student.georgianc.on.ca>
2020-04-11 22:17:05 -0400

    Lab-4 done

---
+++ Models/Manufacturer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhoneApplication.Models
+{
+    public class Manufacturer
+    {
+        public Manufacturer()
+        {
+            URL = "";
+            ManufacturerName = "";
+            DateReleased = new DateTime();
+        }
+        public String ManufacturerName { get; set; }
+        public string URL { get; set; }
+        public DateTime DateReleased { get; set; }
+        [Key]
+        public int ManufacturerID { get; set; }
+
+        public ICollection<Phone> PhoneList { get; set; }
+
+    }
+}
---
+++ Models/Phone.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PhoneApplication.Models
+{
+    public class Phone
+    {
+        public Phone()
+        {
+            PhoneName = "";
+
+            DateReleased = new DateTime();
+
+        }
+        [Key]
+        public int PhoneID { get; set; }
+
+        public String PhoneName { get; set; }
+
+        public virtual Manufacturer Manufacturer { get; set; }
+
+        public double MSRP { get; set; }
+
+        public double ScreenSize { get; set; }
+
+        public DateTime DateReleased { get; set; }
+
+        [ForeignKey("Manufacturer")]
+        public int ManufacturerID { get; set; }
+
+
+
+    }
+}
